b100dac3319cfb18e80edc951e37bc611707842e
Anton Maminov <anton.linux@gmail.com>
2025-05-06 01:55:29 +0300

    Update upload_io_spec.cr

--- spec/upload_io_spec.cr
+++ spec/upload_io_spec.cr
@@ -101,7 +101,7 @@ describe UploadIO do
           uploaded_total += chunk_size
           chunks << chunk_size
         },
-        ->(nil : Nil) {
+        ->(_nil : Nil) {
           read_count += 1
           read_count > 1 # Cancel after first chunk
         }
@@ -130,7 +130,7 @@ describe UploadIO do
           uploaded_total += chunk_size
           chunks << chunk_size
         },
-        ->(nil : Nil) { false }
+        ->(_nil : Nil) { false }
       )
 
       buffer = Bytes.new(4096)
